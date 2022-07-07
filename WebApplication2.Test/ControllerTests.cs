b16ae2c5ec445f80511d52376de517f4ba20fe4e
Matt <Matthew.Kubyshkin@gmail.com>
2022-07-06 21:16:23 -0400

    Added more unit tests

--- WebApplication2.Test/ControllerTests.cs
+++ WebApplication2.Test/ControllerTests.cs
@@ -86,19 +86,114 @@ namespace WebApplication2.Test
             // Assert 
             Assert.True(result == 7);
             logger.Received()
-               .Log(LogLevel.Information, Arg.Is<string>(s => s.Contains("x = 3 y = 4"))); 
+               .Log(LogLevel.Information, Arg.Is<string>(s => s.Contains("x = 3 y = 4")));
         }
-    }
+        // This one 
+        [Fact]
+        public void ControllerFirstElement_ReturnSucess()
+        {
+            // Setup
+            var logger = Substitute.For<ILogger<WeatherForecastController>>();
+            var controller = new WeatherForecastController(logger);
+            int[] b = new int[] { 6, 3, 2 };
+            // Act 
+            var result = controller.FirstElement(b);
 
-    public abstract class MockLogger <T> : ILogger <T>
-    {
-        void ILogger.Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter) =>
-            Log(logLevel, formatter(state, exception));
+            // Assert 
+            Assert.True(result == 6);
+        }
 
-        public abstract void Log(LogLevel logLevel, string message);
+        [Fact]
+        public void ControllerFirstElement_ThrowsException()
+        {
+            // Setup
+            var logger = Substitute.For<ILogger<WeatherForecastController>>();
+            var controller = new WeatherForecastController(logger);
+            int[] b = new int[] { };
 
-        public virtual bool IsEnabled(LogLevel logLevel) => true;
+            // Act 
+            Assert.Throws<IndexOutOfRangeException>(() => controller.FirstElement(b));
+        }
 
-        public abstract IDisposable BeginScope<TState>(TState state);
+        [Fact]
+        public void ControllerFirstElement_LogsAsExpected()
+        {
+            // Setup
+            var logger = Substitute.For<MockLogger<WeatherForecastController>>();
+
+            var controller = new WeatherForecastController(logger);
+            int[] b = new int[] { 6, 3, 2 };
+
+            // Act 
+            var result = controller.FirstElement(b);
+
+            // Assert 
+            Assert.True(result == 6);
+            logger.Received()
+               .Log(LogLevel.Information, Arg.Is<string>(s => s.Contains("Received an array having Length=3")));
+        }
+
+        [Fact]
+        public void ControllerProduct_ReturnSucess()
+        {
+            // Setup
+            var logger = Substitute.For<ILogger<WeatherForecastController>>();
+            var controller = new WeatherForecastController(logger);
+            int x = 1;
+            int y = 2;
+            int z = 3;
+            // Act 
+            var result = controller.Product(x, y, z);
+
+            // Assert 
+            Assert.True(result == 6);
+        }
+
+        [Fact]
+        public void ControllerProduct_ThrowsException()
+        {
+            // Setup
+            ILogger<WeatherForecastController> logger = null;
+            var controller = new WeatherForecastController(logger);
+            int x = 400000000;
+            int y = 500000000;
+            int z = 700000000;
+
+            // Act 
+            Assert.Throws<System.ArgumentNullException>(() => controller.Product(x, y, z));
+
+        }
+
+        [Fact]
+        public void ControllerProduct_LogsAsExpected()
+        {
+            // Setup
+            var logger = Substitute.For<MockLogger<WeatherForecastController>>();
+
+            var controller = new WeatherForecastController(logger);
+            int x = 1;
+            int y = 2;
+            int z = 3;
+
+            // Act 
+            var result = controller.Product(x, y, z);
+
+            // Assert 
+            Assert.True(result == 6);
+            logger.Received()
+               .Log(LogLevel.Information, Arg.Is<string>(s => s.Contains("x = 1 y = 2 z = 3")));
+        }
+
+        public abstract class MockLogger<T> : ILogger<T>
+        {
+            void ILogger.Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter) =>
+                Log(logLevel, formatter(state, exception));
+
+            public abstract void Log(LogLevel logLevel, string message);
+
+            public virtual bool IsEnabled(LogLevel logLevel) => true;
+
+            public abstract IDisposable BeginScope<TState>(TState state);
+        }
     }
 }
